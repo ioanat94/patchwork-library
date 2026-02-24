4a57f628d335add5a0a84400247f3e4c35d59686
Abhishek khati chhetri <abhichhetri@outlook.com>
2026-02-24 15:07:04 +0200

    fix: added target blank to open url in new window

--- src/components/ResourceItem.tsx
+++ src/components/ResourceItem.tsx
@@ -1,7 +1,7 @@
-import { Badge } from '@/components/ui/badge';
-import Header from './Header';
-import Library from './Library';
-import type { Resource } from '@/utils/types';
+import { Badge } from "@/components/ui/badge";
+import Header from "./Header";
+import Library from "./Library";
+import type { Resource } from "@/utils/types";
 
 type ResourceItemProps = {
   resource: Resource;
@@ -9,19 +9,20 @@ type ResourceItemProps = {
 
 export default function ResourceItem({ resource }: ResourceItemProps) {
   return (
-    <div className='border-b border-slate-200 py-4 last:border-b-0'>
-      <div className='flex flex-col gap-2'>
+    <div className="border-b border-slate-200 py-4 last:border-b-0">
+      <div className="flex flex-col gap-2">
         <a
           href={resource.url}
-          rel='noopener noreferrer'
-          className='text-5xl font-semibold text-blue-600 hover:text-blue-800 hover:underline'
+          rel="noopener noreferrer"
+          target="_blank"
+          className="text-5xl font-semibold text-blue-600 hover:text-blue-800 hover:underline"
         >
           {resource.title}
         </a>
-        <p className='text-sm text-slate-600 break-all'>{resource.url}</p>
-        <div className='flex flex-wrap gap-2'>
+        <p className="text-sm text-slate-600 break-all">{resource.url}</p>
+        <div className="flex flex-wrap gap-2">
           {resource.tags.map((tag) => (
-            <Badge key={resource.id} variant='secondary' className='text-white'>
+            <Badge key={resource.id} variant="secondary" className="text-white">
               {tag}
             </Badge>
           ))}
