91625aac9884366c68afdd96456316da9c350ad0
Abhishek khati chhetri <abhichhetri@outlook.com>
2026-02-24 16:14:48 +0200

    feat: added name to contributor

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -1,16 +1,22 @@
-import type { Contributor } from '@/utils/types';
+import type { Contributor } from "@/utils/types";
 
 export const contributors: Contributor[] = [
   {
-    'id': 'c0a80101-0000-0000-0000-000000000001',
-    'name': 'Ioana',
-    'github': 'ioanat94',
-    'contributions': 'Project creator and initial setup',
+    id: "c0a80101-0000-0000-0000-000000000001",
+    name: "Ioana",
+    github: "ioanat94",
+    contributions: "Project creator and initial setup",
   },
   {
-  "id": "2a4bcad8-3452-481a-bc57-cef34de8e324",
-  "name": "Kurpotato",
-  "github": "Kurpotato",
-  "contributions": "Little UX test"
-  }
+    id: "2a4bcad8-3452-481a-bc57-cef34de8e324",
+    name: "Kurpotato",
+    github: "Kurpotato",
+    contributions: "Little UX test",
+  },
+  {
+    id: "8ee0d2f2-7f12-4dc2-bb88-89b077b92960",
+    name: "Abhishek khati chhetri",
+    github: "https://github.com/abhichhetri09",
+    contributions: "Bug fixes",
+  },
 ];
